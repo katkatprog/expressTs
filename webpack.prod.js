3b8f560aef7d21d3e87c43d22af88cf775873836
katkatprog <katkatprog@gmail.com>
2022-06-07 00:25:48 +0900

    clean-webpack-pluginの導入

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,25 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-  mode:'production',
-  entry:'./src/frontend/index.ts',//エントリーポイント
-  output:{
-    filename:'bundle.js',//出力するファイル名
-    path: path.resolve(__dirname, 'public','javascripts')//ファイル出力先ディレクトリ
+  mode: 'production',
+  entry: './src/frontend/index.ts',//エントリーポイント
+  output: {
+    filename: 'bundle.js',//出力するファイル名
+    path: path.resolve(__dirname, 'public', 'javascripts')//ファイル出力先ディレクトリ
   },
-  module:{
+  module: {
     rules: [{
       test: /\.ts$/,
       use: 'ts-loader',
       exclude: /node_modules/
     }]
   },
-  resolve:{
-    extensions:['.ts','.js']
+  resolve: {
+    extensions: ['.ts', '.js']
     //typescriptでは、import時に拡張子書かない。しかしそれだけではwebpackエラー出る。そのための拡張子の補完。
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
